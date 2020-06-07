10616e2da9939fc7013d8646db0d34bc430a8df1
biswal19 <chandrikabiswal1@gmail.com>
2020-06-07 04:39:08 -0700

    Updates

---
+++ precache-manifest.f617c99b472ff819528568f05b663d06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e693d18d851e8166aa4bc6286e3d2ba9",
+    "url": "/react-note-taker-app/index.html"
+  },
+  {
+    "revision": "73cc9396667a7316d0b6",
+    "url": "/react-note-taker-app/static/css/main.cbe573b7.chunk.css"
+  },
+  {
+    "revision": "94fa8815841da92800fe",
+    "url": "/react-note-taker-app/static/js/2.3aa80256.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-note-taker-app/static/js/2.3aa80256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73cc9396667a7316d0b6",
+    "url": "/react-note-taker-app/static/js/main.2964518f.chunk.js"
+  },
+  {
+    "revision": "73dcb7ef108bab6bfef9",
+    "url": "/react-note-taker-app/static/js/runtime-main.e1f5c4c3.js"
+  }
+]);
